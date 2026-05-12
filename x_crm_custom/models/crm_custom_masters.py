bdc217dde76ae242bff8be51aa519487c7be04b7
glassesboyy <aliperwira26@gmail.com>
2026-05-12 08:03:46 +0700

    feat: customize field crm; still need test and enhancement

---
+++ x_crm_custom/models/crm_custom_masters.py
@@ -0,0 +1,64 @@
+from odoo import models, fields
+
+class CrmClientType(models.Model):
+    _name = 'crm.client.type'
+    _description = 'CRM Client Type'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmTujuan(models.Model):
+    _name = 'crm.tujuan'
+    _description = 'CRM Tujuan'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmJenisTransaksi(models.Model):
+    _name = 'crm.jenis.transaksi'
+    _description = 'CRM Jenis Transaksi'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmSourceCustom(models.Model):
+    _name = 'crm.source.custom'
+    _description = 'CRM Source Custom'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmJenisKendaraan(models.Model):
+    _name = 'crm.jenis.kendaraan'
+    _description = 'CRM Jenis Kendaraan'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmSumberDaya(models.Model):
+    _name = 'crm.sumber.daya'
+    _description = 'CRM Sumber Daya'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmPenggunaanKendaraan(models.Model):
+    _name = 'crm.penggunaan.kendaraan'
+    _description = 'CRM Penggunaan Kendaraan'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmRentalType(models.Model):
+    _name = 'crm.rental.type'
+    _description = 'CRM Rental Type'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
+
+class CrmUsageLocation(models.Model):
+    _name = 'crm.usage.location'
+    _description = 'CRM Usage Location'
+
+    name = fields.Char(string='Name', required=True)
+    active = fields.Boolean(default=True)
