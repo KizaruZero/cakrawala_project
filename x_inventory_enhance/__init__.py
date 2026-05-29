c61f391961e82b33936eed1b4f568c4826cd72a9
DickyCandraPermana <dickycandrapermana@student.uns.ac.id>
2026-05-29 10:05:36 +0700

    feat: implement access control for stock picking types with default admin permissions

--- x_inventory_enhance/__init__.py
+++ x_inventory_enhance/__init__.py
@@ -1,1 +1,5 @@
 from . import models
+
+
+def post_init_hook(env):
+    env["stock.picking.type"]._set_default_allowed_admin()
--- x_inventory_enhance/models/stock_picking_type.py
+++ x_inventory_enhance/models/stock_picking_type.py
@@ -1,10 +1,23 @@
-from odoo import fields, models
+from odoo import api, fields, models
 from odoo.exceptions import AccessError
-from odoo.fields import Domain
+from odoo.fields import Command, Domain
 
 
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
+
+    def _default_allowed_user_ids(self):
+        admin = self.env.ref("base.user_admin", raise_if_not_found=False)
+        return [Command.link(admin.id)] if admin else False
+
+    @api.model
+    def _set_default_allowed_admin(self):
+        admin = self.env.ref("base.user_admin", raise_if_not_found=False)
+        if not admin:
+            return
+
+        picking_types = self.sudo().search([("allowed_user_ids", "=", False)])
+        picking_types.write({"allowed_user_ids": [Command.link(admin.id)]})
 
     allowed_user_ids = fields.Many2many(
         "res.users",
@@ -12,14 +25,15 @@ class StockPickingType(models.Model):
         "picking_type_id",
         "user_id",
         string="Allowed Users",
+        default=_default_allowed_user_ids,
         domain=[("share", "=", False)],
-        help="If empty, every inventory user can access this operation type. "
-             "If filled, only listed users can see and open it in Inventory Overview and related transfer actions.",
+        help="Only listed users can see and open this operation type in Inventory Overview "
+             "and related transfer actions. New operation types default to Administrator only.",
     )
 
     def _is_allowed_for_current_user(self):
         self.ensure_one()
-        return not self.allowed_user_ids or self.env.user in self.allowed_user_ids
+        return self.env.user in self.allowed_user_ids
 
     def _check_allowed_for_current_user(self):
         for picking_type in self:
@@ -48,30 +62,18 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def _operation_type_allowed_domain(self):
-        return [
-            "|",
-            ("picking_type_id.allowed_user_ids", "=", False),
-            ("picking_type_id.allowed_user_ids", "in", [self.env.uid]),
-        ]
+        return [("picking_type_id.allowed_user_ids", "in", [self.env.uid])]
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     def _operation_type_allowed_domain(self):
-        return [
-            "|",
-            ("picking_type_id.allowed_user_ids", "=", False),
-            ("picking_type_id.allowed_user_ids", "in", [self.env.uid]),
-        ]
+        return [("picking_type_id.allowed_user_ids", "in", [self.env.uid])]
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     def _operation_type_allowed_domain(self):
-        return [
-            "|",
-            ("picking_type_id.allowed_user_ids", "=", False),
-            ("picking_type_id.allowed_user_ids", "in", [self.env.uid]),
-        ]
+        return [("picking_type_id.allowed_user_ids", "in", [self.env.uid])]
