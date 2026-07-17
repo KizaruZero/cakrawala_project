adcb3a1e92539699c488869ec9714806278eef7b
Doni Hadiansyah <Doni.Hadiansyah@xapiens.id>
2026-07-17 08:15:50 +0700

    Update: - Modify table special request

--- x_rental_profit_calculation/migrations/19.0.1.0.24/post-migrate.py
+++
@@ -1,9 +0,0 @@
-from odoo import SUPERUSER_ID, api
-
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    documents = env['rpc.document'].search([
-        ('state', 'in', ('finance_done', 'approved')),
-    ])
-    documents._generate_finance_lines()
