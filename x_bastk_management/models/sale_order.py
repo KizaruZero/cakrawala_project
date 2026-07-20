0a1c62e645933f2ffabc0aff6d5242019fff8415
DickyCandraPermana <dickycandrapermana@student.uns.ac.id>
2026-07-20 10:29:00 +0700

    add duration field to replacement car, fix sales order error

--- x_bastk_management/models/sale_order.py
+++ x_bastk_management/models/sale_order.py
@@ -5,6 +5,11 @@ class SaleOrderLine(models.Model):
 
     def _prepare_procurement_values(self, **kwargs):
         vals = super()._prepare_procurement_values(**kwargs)
-        if self.order_id.analytic_account_id:
-            vals['analytic_account_id'] = self.order_id.analytic_account_id.id
+        if self.analytic_distribution:
+            vals['analytic_distribution'] = self.analytic_distribution
+            try:
+                first_account_id = next(iter(self.analytic_distribution.keys()))
+                vals['analytic_account_id'] = int(first_account_id)
+            except Exception:
+                pass
         return vals
