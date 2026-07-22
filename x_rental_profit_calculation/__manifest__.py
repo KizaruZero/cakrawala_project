00d44c57a7b296aa20db56e317f71a0863755c89
Doni Hadiansyah <Doni.Hadiansyah@xapiens.id>
2026-07-23 06:33:31 +0700

    Update: - Fixing label and fixing formula

--- x_rental_profit_calculation/__manifest__.py
+++ x_rental_profit_calculation/__manifest__.py
@@ -1,7 +1,7 @@
 ## -*- coding: utf-8 -*-
 {
     'name': 'Rental Profit Calculation',
-    'version': '19.0.1.0.30',
+    'version': '19.0.1.0.32',
     'category': 'Sales/Rental',
     'summary': 'Rental Profit Calculation (RPC) for PT Cakrawala Rentalindo Sejahtera',
     'description': """
