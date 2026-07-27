3d9f88250e6958f164b3837c71bc2bcd4f55fe7b
glassesboyy <aliperwira26@gmail.com>
2026-07-27 11:31:10 +0700

    ignore script dan hapus file yang gaperlu di commit

--- find_free_service.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='localhost', port='5432', user='odoo', password='odoo', dbname='cakrawala_dev')
-cur = conn.cursor()
-cur.execute("UPDATE ir_ui_view SET active = False WHERE id IN (2685, 2686);")
-conn.commit()
-print('Deactivated.')
--- find_sub_type_id.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(host='localhost', port='5432', user='odoo', password='odoo', dbname='cakrawala_dev')
-cur = conn.cursor()
-cur.execute("UPDATE ir_ui_view SET active = False WHERE id = 2610;")
-conn.commit()
-print('View 2610 deactivated.')
