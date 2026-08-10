e1f65e30604d0fd0c1dd239753a8456be6c9b199
Doni Hadiansyah <Doni.Hadiansyah@xapiens.id>
2026-08-10 09:45:24 +0700

    Update: - Add HOK tab and all field and formulas

--- x_rental_profit_calculation/__manifest__.py
+++ x_rental_profit_calculation/__manifest__.py
@@ -1,7 +1,7 @@
 ## -*- coding: utf-8 -*-
 {
     'name': 'Rental Profit Calculation',
-    'version': '19.0.1.0.48',
+    'version': '19.0.1.0.49',
     'category': 'Sales/Rental',
     'summary': 'Rental Profit Calculation (RPC) for PT Cakrawala Rentalindo Sejahtera',
     'description': """
